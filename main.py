b05a7f3760cffed6b8837a92e9bb06a5c41b2375
Jacob Ledbetter <ledbetterte@gmail.com>
2022-04-10 21:15:14 -0400

    Refactored it all. Much better... |)

--- main.py
+++ main.py
@@ -3,158 +3,134 @@ import re
 import webbrowser
 import copy
 import json
+from dataclasses import dataclass, field
 
 
-def get_responses(start, end):
-    n = start
-    response_list = {}
-    while n <= end:
-        r = requests.get("https://www.tokywoky.com/webview/v4/{}".format(n))
-        print("Got {} with code {}".format(n, r.status_code))
-        response_list[n] = r
-        n += 1
-    return response_list
+@dataclass
+class toky:
+    link_id: int
+    link: str
+    response: requests.Response
+    group_info: dict = field(default_factory=dict)
+    category_id: int = -1
+    questions: dict = field(default_factory=dict)
+    question_count: int = -1
+
+    def populate_group_info(self):
+        """Get the group info for the group from a response."""
+        content = self.response.content.decode("utf-8")
+        self.group_info = json.loads(re.search(r"window\.group = ({.*})", content).group(1))
+        self.category_id = self.group_info["groupCategoryId"]
+
+    def populate_questions(self):
+        """Get the questions of a toky."""
+        r = requests.get("https://www.tokywoky.com/api/v2/brandcategories/{}/questions".format(self.category_id))
+        try:
+            self.questions = r.json()
+            self.question_count = self.questions.__len__()
+        except json.JSONDecodeError:
+            pass
 
 
-def clean_by_response_code(response_list):
-    rm_key_list = []
-    for k in response_list.keys():
-        if not response_list[k].ok:
-            rm_key_list.append(k)
-    for k in rm_key_list:
-        response_list.pop(k)
-    return response_list
+
+def get_tokys(start, end, debug=False):
+    """Get tokys by link_Id from start(inclusive) to end(exclusive)."""
+    tokys = []
+    for n in range(start, end):
+        r = requests.get(f"https://www.tokywoky.com/webview/v4/{n}")
+        if debug:
+            print(f"Got {n} with code {r.status_code}")
+        tokys.append(toky(n, f"https://www.tokywoky.com/webview/v4/{n}", r))
+    return tokys
 
 
-def clean_by_content_search(response_list):
-    rm_key_list = []
-    for k in response_list.keys():
-        if re.search(r"<div id=\"app\"></div>", response_list[k].content.decode("utf-8")) is None:
-            rm_key_list.append(k)
-    for k in rm_key_list:
-        response_list.pop(k)
-    return response_list
+def clean_by_response_code(tokys):
+    """Remove tokys from the list if they have a response that is not ok"""
+    for toky_index in reversed(range(tokys.__len__())):
+        if not tokys[toky_index].response.ok:
+            tokys.pop(toky_index)
+    return tokys
 
 
-def convert_id_to_link(link_id):
-    return "https://www.tokywoky.com/webview/v4/{}".format(link_id)
-
-
-def convert_to_links(response_list):
-    links_list = copy.copy(response_list)
-    for k in links_list.keys():
-        links_list[k] = convert_id_to_link(k)
-    return links_list
+def convert_to_links(tokys):
+    """Return a list of links from the  ids in the response_list."""
+    return [toky.link for toky in tokys]
 
 
 def open_all_links(links_list):
+    """Take a list of links and open them all in the default browser."""
     first = True
-    if isinstance(links_list, dict):
-        for k in links_list.keys():
-            if first:
-                webbrowser.open_new(links_list[k])
-                first = False
-            else:
-                webbrowser.open_new_tab(links_list[k])
-    else:
-        for k in links_list:
-            if first:
-                webbrowser.open_new(k)
-                first = False
-            else:
-                webbrowser.open_new_tab(k)
+    for k in links_list:
+        if first:
+            webbrowser.open_new(k)
+            first = False
+        else:
+            webbrowser.open_new_tab(k)
 
 
-def export_links(links_list):
+def export_links(links, file_name=None):
+    """Take a list of links and return them as a newline separated string."""
     s = ""
-    for k in links_list.keys():
-        s += links_list[k] + "\n"
+    for link in links:
+        s += link + "\n"
+    if file_name is not None:
+        with open(file_name, r"w") as f:
+            f.write(s)
     return s
 
 
-def close_all_responses(response_list):
-    for r in response_list.values():
-        if isinstance(r, list):
-            r[0].close()
-        else:
-            r.close()
+def close_all_responses(tokys):
+    """Close all the responses in the response list."""
+    for toky in tokys:
+        toky.response.close()
 
 
 def import_links(links_string):
+    """Convert a newline separated list of links to a list."""
     return links_string.split("\n")
 
 
-def getGroupInfo(response):
-    content = response.content.decode("utf-8")
-    return json.loads(re.search(r"window\.group = ({.*})", content).group(1))
+def separate_responses(tokys):
+    """
+    Separate tokys into lists using some criteria.
 
-
-def getQuestions(categoryid):
-    r = requests.get("https://www.tokywoky.com/api/v2/brandcategories/{}/questions".format(categoryid))
-    try:
-        return r.json()
-    except json.JSONDecodeError:
-        return "NoQuestions"
-
-
-def addQuestionCount(response_list):
-    for link_id in response_list:
-        q = getQuestions(getGroupInfo(response_list[link_id])["groupCategoryId"])
-        if q == "NoQuestions":
-            response_list[link_id] = [response_list[link_id], q]
+    Tokys are safe if they have between 0 and 9.
+    Tokys are in_use if they have 10 or more questions.
+    Tokys need investigation if they have -1 questions(didn't get json from api)
+    """
+    investigate_tokys = []
+    in_use_tokys = []
+    safe_tokys = []
+    for toky in tokys:
+        if toky.question_count == -1:
+            investigate_tokys.append(toky)
+        elif toky.question_count >= 10:
+            in_use_tokys.append(toky)
         else:
-            response_list[link_id] = [response_list[link_id], q.__len__()]
-    return response_list
+            safe_tokys.append(toky)
+    return investigate_tokys, in_use_tokys, safe_tokys
 
 
-def printIdAndQCount(response_list):
-    for link_id in response_list:
-        print("{}: {}".format(convert_id_to_link(link_id), response_list[link_id][1]))
+def populate_all(tokys, debug=False):
+    """Populate group info and questions for a list of tokys"""
+    for toky in tokys:
+        if debug:
+            print(f"populating {toky.link}")
+        toky.populate_group_info()
+        toky.populate_questions()
 
 
-def clean_by_questions(response_list):
-    rm_key_list = []
-    for link_id in response_list:
-        if response_list[link_id][1] == 'NoQuestions':
-            continue
-        if response_list[link_id][1] > 2:  # Remove if...
-            rm_key_list.append(link_id)
-    for k in rm_key_list:
-        response_list.pop(k)
-    return response_list
-
-
-def separateResponses(response_list):
-    investigate_response_list = {}
-    for link_id in response_list:
-        if response_list[link_id][1] == 'NoQuestions':
-            investigate_response_list[link_id] = response_list[link_id]
-    for link_id in investigate_response_list:
-        response_list.pop(link_id)
-    return response_list, investigate_response_list
-
-
-if __name__ == "__main__":
-    rs = get_responses(0, 530)
-    rs = clean_by_response_code(rs)
-    rs = clean_by_content_search(rs)
-    rs = addQuestionCount(rs)
-    rs = clean_by_questions(rs)
-    rs, irs = separateResponses(rs)
-    ls = convert_to_links(rs)
-    ils = convert_to_links(irs)
-    print("\n--------------------\n")
-    print(export_links(ls))
-    print("\n-----investigate----\n")
-    print(export_links(ils))
-    open_all_links(ils)
-    # open_all_links(ls)
-    close_all_responses(rs)
-    close_all_responses(irs)
-    # bc 605 url toky.chat
-    # bc 20 url 74 (lovecore)
-    # bc 101 url 131 (gray)
-    # bc 607 url 515 (goth)
+if __name__ == "__main__" and True:
+    ts = get_tokys(0, 600, debug=True)
+    print(f"Unclean: {[t.link_id for t in ts]}")
+    clean_by_response_code(ts)
+    print(f"Clean:   {[t.link_id for t in ts]}")
+    populate_all(ts, debug=True)
+    it, ut, st = separate_responses(ts)
+    print(f"\n -------------investigate------------- \n{export_links(convert_to_links(it), 'tokys_investigate.txt')}")
+    print(f"\n ---------------in use---------------- \n{export_links(convert_to_links(ut), 'tokys_in_use.txt')}")
+    print(f"\n ----------------safe----------------- \n{export_links(convert_to_links(st), 'tokys_safe.txt')}")
+    close_all_responses(ts)
 
     # some api stuff
     # questions, customerServiceKeywords, chats(accepts a query), emojis, users
